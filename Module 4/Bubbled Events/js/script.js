b1f7df2538ced9d568d846a703ce3a9c298dc7b1
RicardoMarinCarsi <ricardomarincarsi@hotmail.com>
2017-09-20 23:41:02 +0200

    OK

---
+++ Module 4/Bubbled Events/js/script.js
@@ -0,0 +1,15 @@
+var innerMostElement = document.getElementsByClassName('innermost')[0];
+var sections = document.getElementsByTagName('section');
+
+for (var  index=0;index<sections.length;++index) {
+    sections[index].onclick=handleClick;
+}
+
+
+function handleClick (event) {
+    console.log('current element: ' + this.className + ' | target element: ' + event.target.className);
+    if (this.className=='regular') {
+        event.stopPropagation();
+        console.log('Event progragation stopped');
+    }
+}
