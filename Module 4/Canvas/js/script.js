689b9c8319ebabcd67e0c8d5a34fd17d2ca50e61
RicardoMarinCarsi <ricardomarincarsi@hotmail.com>
2017-09-23 22:13:37 +0200

    OK

--- Module 4/Canvas/js/script.js
+++ Module 4/Canvas/js/script.js
@@ -14,3 +14,15 @@ context.arc(160,80,70,0,2*Math.PI);
 context.fill();
 context.stroke();
 
+context.beginPath();
+context.moveTo(650,345);
+context.lineTo(700, 350);
+context.lineTo(665, 315);
+context.stroke();
+
+context.fillStyle = "#20293F";
+context.strokeStyle = "#20293F";
+
+context.font = "78px Segoe UI";
+context.fillText("Hello",190,230)
+context.strokeText("World",190,310);
